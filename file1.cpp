c052750f43f174457c4e7e96e125a29cd118d284
meilan <meilanulfia@gmail.com>
2025-03-18 20:22:17 +0700

    Menambahkan increment pecacah naik

--- file1.cpp
+++ file1.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int main() {
+    //Pecahan naik
+    int i;
+    for (i = 0; i < 5; i++){
+        cout << "Nilai i = "<< i <<" Lari Keliling Lapangan" << endl;
+    }
+    cout << "Nilai i terakhir : " << i << endl;
+
+   
+}
